5319752bd61bf4b1cd94908414ade242dd57efff
heitor <124824065+heitordelbuono@users.noreply.github.com>
2025-12-02 23:36:38 -0300

    Final adaptado para deploy no Render 7.0

--- setup/settings.py
+++ setup/settings.py
@@ -110,12 +110,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 if not DEBUG:
-    # Configurações para o Render (Produção)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Uploads)
