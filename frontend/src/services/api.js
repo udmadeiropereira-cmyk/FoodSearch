4dafb6e3ccceb29c910b0620f4ce5a3f710ee664
heitor <124824065+heitordelbuono@users.noreply.github.com>
2025-12-03 01:07:42 -0300

    Final adaptado para deploy no Render 10.0

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://foodsearch-api.onrender.com",
+  baseURL: "https://foodsearch-api.onrender.com/api",
 });
 
 export default api;
